e9896250fa6e48d0a26aee8ebce1c4d3291ed515
ITAnt <964161472@qq.com>
2018-12-05 18:59:45 +0800

    init

---
+++ app/src/main/java/com/itant/scaletypetest/CenterInsideActivity.java
@@ -0,0 +1,13 @@
+package com.itant.scaletypetest;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class CenterInsideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_center_inside);
+    }
+}
---
+++ app/src/main/java/com/itant/scaletypetest/MainActivity.java
@@ -0,0 +1,60 @@
+package com.itant.scaletypetest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        findViewById(R.id.btn_fit_center).setOnClickListener(this);
+        findViewById(R.id.btn_center).setOnClickListener(this);
+        findViewById(R.id.btn_center_crop).setOnClickListener(this);
+        findViewById(R.id.btn_center_inside).setOnClickListener(this);
+        findViewById(R.id.btn_fit_end).setOnClickListener(this);
+        findViewById(R.id.btn_fit_start).setOnClickListener(this);
+        findViewById(R.id.btn_fit_xy).setOnClickListener(this);
+        findViewById(R.id.btn_matrix).setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.btn_fit_center:
+                startActivity(new Intent(this, FitCenterActivity.class));
+                break;
+
+            case R.id.btn_center:
+                startActivity(new Intent(this, CenterActivity.class));
+                break;
+
+            case R.id.btn_center_crop:
+                startActivity(new Intent(this, CenterCropActivity.class));
+                break;
+
+            case R.id.btn_center_inside:
+                startActivity(new Intent(this, CenterInsideActivity.class));
+                break;
+
+            case R.id.btn_fit_end:
+                startActivity(new Intent(this, FitEndActivity.class));
+                break;
+
+            case R.id.btn_fit_start:
+                startActivity(new Intent(this, FitStartActivity.class));
+                break;
+
+            case R.id.btn_fit_xy:
+                startActivity(new Intent(this, FitXYActivity.class));
+                break;
+
+            case R.id.btn_matrix:
+                startActivity(new Intent(this, MatrixActivity.class));
+                break;
+        }
+    }
+}
